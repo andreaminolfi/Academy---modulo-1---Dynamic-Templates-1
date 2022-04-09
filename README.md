ed6cf6d1367281f52bd4cb83a2a1f19f18cc4fe8
andreaminolfi <andrea@minolfi.it>
2022-04-09 18:20:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ds4tg7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ds4tg7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  submitHandler(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+}
